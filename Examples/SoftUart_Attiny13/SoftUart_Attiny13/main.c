c731a141e0ea9919cab4a174bad1c9f55d8d1a80
Yahor Halubchyk <yahor.halubchyk@itechart-group.com>
2018-04-18 10:57:01 +0300

    Add main-code to SoftUart_Attiny13

--- Examples/SoftUart_Attiny13/SoftUart_Attiny13/main.c
+++ Examples/SoftUart_Attiny13/SoftUart_Attiny13/main.c
@@ -5,14 +5,49 @@
  * Author : yahor.halubchyk
  */ 
 
+#define F_CPU 9600UL*1000
 #include <avr/io.h>
+#include <extensions.h>
 
+
+//************** soft uart
+#define USOFT_BAUD 4800
+#define USOFT_IO_RX B, 4
+#define USOFT_IO_TX B, 3
+#define USOFT_BUFFER_EN false
+#include <uart_soft.h>
+
+unsigned char myByte;
+bool isNew = false;
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	usoft_init();
+
+	//test RX
+	while(1)
+	{
+	  usoft_putStringf("test uart");
+	  usoft_putCharf(0x0D); //end line
+	  delay_ms(500);
+	}
+
+	//test TX
+	while(1)
+	{
+		usoft_listen();
+		if (isNew)
+		{
+			usoft_putChar(myByte);
+			isNew = false;
+		}
+	}
 }
+
+USOFT_ISR_newByte(b)
+{
+    isNew = true;
+	myByte = b;
+}
+
 
