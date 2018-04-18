df3c8c907c39e5fd221f203ecf1cb2b2acc77cdf
Yahor Halubchyk <yahor.halubchyk@itechart-group.com>
2018-04-18 10:40:41 +0300

    Add SoftUart_Attiny13 to Examples

---
+++ Examples/SoftUart_Attiny13/SoftUart_Attiny13/main.c
@@ -0,0 +1,18 @@
+/*
+ * SoftUart_Attiny13.c
+ *
+ * Created: 18-Apr-18 10:33:47
+ * Author : yahor.halubchyk
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
