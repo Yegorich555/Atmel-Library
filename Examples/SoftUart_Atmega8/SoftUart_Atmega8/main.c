d6e1084f3cc2a1f26de4ffb6c69ab839364ffc23
Yahor Halubchyk <yahor.halubchyk@itechart-group.com>
2018-04-19 17:32:18 +0300

    Add main-code to SoftUart_Atmega8

--- Examples/SoftUart_Atmega8/SoftUart_Atmega8/main.c
+++ Examples/SoftUart_Atmega8/SoftUart_Atmega8/main.c
@@ -5,14 +5,63 @@
  * Author : yahor.halubchyk
  */ 
 
+#define F_CPU 8000UL*1000
 #include <avr/io.h>
+#include <extensions.h>
+#include <avr/interrupt.h>
 
+#define IO_DBG_OUT D, 4
+
+//************** soft uart
+#define USOFT_BAUD 4800
+#define USOFT_IO_RX D, 3
+#define USOFT_IO_TX D, 2
+#define USOFT_TXEN true
+#define USOFT_RXEN true
+#define USOFT_BUFFER_EN true
+#define USOFT_BUFFER_SIZE 4
+//#define USOFT_AUTOLISTEN true
+#include <uart_soft.h>
+
+unsigned char myByte;
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	usoft_init();
+	
+	io_set(DDR, IO_DBG_OUT);
+
+	//test RX
+	while(1)
+	{
+		usoft_putStringf("test uart");
+		usoft_putCharf(0x0D); //end line
+		delay_ms(500);
+		break;
+	}
+
+	//test TX
+	while(1)
+	{
+		while(usoft_rxCounter<3)
+		{
+			usoft_listen();
+		}
+
+		if (!usoft_rx_work && usoft_rxCounter)
+		{
+			delay_ms(500);
+			usoft_putChar(usoft_rxBuffer[0]);
+			usoft_putChar(usoft_rxBuffer[1]);
+			usoft_putChar(usoft_rxBuffer[2]);
+			usoft_putChar(usoft_rxCounter);
+			usoft_putChar(myByte);
+			usoft_rxCounter = 0;
+		}
+	}
 }
 
+USOFT_ISR_newByte(b)
+{
+	myByte = b;
+}
