cf496bd94846b0ae095da1035b25c8c38f2550ca
Yahor Halubchyk <yahor.halubchyk@itechart-group.com>
2018-04-19 17:09:32 +0300

    Add SoftUart_Atmega16 project to Examples

---
+++ Examples/SoftUart_Atmega16/SoftUart_Atmega16/main.c
@@ -0,0 +1,18 @@
+/*
+ * SoftUart_Atmega16.c
+ *
+ * Created: 19-Apr-18 17:08:34
+ * Author : yahor.halubchyk
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
