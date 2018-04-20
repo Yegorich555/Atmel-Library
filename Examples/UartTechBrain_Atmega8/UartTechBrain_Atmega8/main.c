4edca23af453d3d43b603b80bbf54818f43546ab
Yahor Halubchyk <yahor.halubchyk@itechart-group.com>
2018-04-20 09:36:55 +0300

    Add UartTechBrain_Atmega8 to Examples

---
+++ Examples/UartTechBrain_Atmega8/UartTechBrain_Atmega8/main.c
@@ -0,0 +1,18 @@
+/*
+ * UartTechBrain_Atmega8.c
+ *
+ * Created: 20-Apr-18 09:29:18
+ * Author : yahor.halubchyk
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
