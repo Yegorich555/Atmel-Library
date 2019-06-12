69c3676a7d19b73ee841e1ceda980c80ba3758f1
Yegor G <yegorich_555@mail.ru>
2019-06-12 18:14:33 +0300

    Examples. uart_soft. Change ports according to debug.

--- Examples/SoftUart_Attiny13/SoftUart_Attiny13/main.c
+++ Examples/SoftUart_Attiny13/SoftUart_Attiny13/main.c
@@ -10,11 +10,11 @@
 #include <extensions.h>
 
 //************** soft uart
-//#define USOFT_IO_MEANDR B, 1
+#define USOFT_IO_MEANDER B, 3
 
 #define USOFT_BAUD 4800
-#define USOFT_IO_RX B, 4
-#define USOFT_IO_TX B, 3
+#define USOFT_IO_RX IO_DEBUG_RX
+#define USOFT_IO_TX IO_DEBUG_TX
 #define USOFT_TXEN true
 #define USOFT_BUFFER_EN true
 #define USOFT_BUFFER_SIZE 14
@@ -26,37 +26,37 @@ unsigned char myByte;
 bool isEnd;
 int main(void)
 {
-	MCUCR &= ~(1<<ISC01) | ~(0<<ISC00);	// Trigger INT0 on rising edge
-	PCMSK |= (1<<PCINT4);   // pin change mask: listen to portb, pin PB3
-	GIMSK |= (1<<PCIE); // enable PCINT interrupt
+	//MCUCR &= ~(1<<ISC01) | ~(0<<ISC00);	// Trigger INT0 on rising edge
+	//PCMSK |= (1<<PCINT4);   // pin change mask: listen to portb, pin PB3
+	//GIMSK |= (1<<PCIE); // enable PCINT interrupt
 	
 	usoft_init();
 	usoft_putStringf("Test\r\n");
 	
-	//test TX
-	while(1) // for #define USOFT_AUTOLISTEN true
-	{
-		if (!usoft_rx_work && usoft_rxCounter)
-		{
-			delay_ms(10);
-			if (!usoft_rx_work && usoft_rxCounter)
-			{
-				for (uint8_t i=0;i<usoft_rxCounter;++i)
-				{
-					usoft_putChar(usoft_rxBuffer[i]);
-				}
-				if (myByte)
-				{
-					usoft_putChar(myByte);
-				}
-				usoft_rxCounter = 0;
-			}
-		}
-	}
+	//test RX
+	//while(1) // for #define USOFT_AUTOLISTEN true
+	//{
+		//if (!usoft_rx_work && usoft_rxCounter)
+		//{
+			//delay_ms(10);
+			//if (!usoft_rx_work && usoft_rxCounter)
+			//{
+				//for (uint8_t i=0;i<usoft_rxCounter;++i)
+				//{
+					//usoft_putChar(usoft_rxBuffer[i]);
+				//}
+				//if (myByte)
+				//{
+					//usoft_putChar(myByte);
+				//}
+				//usoft_rxCounter = 0;
+			//}
+		//}
+	//}
 	
 	while(1) // for #define USOFT_AUTOLISTEN false
 	{
-		//usoft_listen();
+		usoft_listen();
 		if (isEnd)
 		{
 			isEnd = false;
@@ -81,8 +81,8 @@ USOFT_ISR_newByte(b)
 		isEnd = true;
 	}
 }
-
-ISR(PCINT0_vect)
-{
-	usoft_listen();
-}
+//
+//ISR(PCINT0_vect)
+//{
+	//usoft_listen();
+//}
